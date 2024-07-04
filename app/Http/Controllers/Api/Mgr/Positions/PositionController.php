e17b0ab2bb64cd3f3eaa52729a4b7b529ea8a59f
osama ibrahim <smsmmarzok30@gmail.com>
2024-07-04 16:34:52 +0300

    position Resource

--- app/Http/Controllers/Api/Mgr/Positions/PositionController.php
+++ app/Http/Controllers/Api/Mgr/Positions/PositionController.php
@@ -3,22 +3,42 @@
 namespace App\Http\Controllers\Api\Mgr\Positions;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\Position\PositionResource;
 use App\Models\Position;
 use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
 
 class PositionController extends Controller
 {
+    /**
+     * @return AnonymousResourceCollection
+     */
     public function index()
     {
-        return(
+        return PositionResource::collection(
             Position::all()
-        );
+        )->additional([
+            'message' => __('Positions retrieved successfully'),
+            'status' => Response::HTTP_OK
+        ]);
     }
 
+    /**
+     * @param Position $position
+     * @return PositionResource
+     */
     public function show(
         Position $position
     )
     {
-        return ($position);
+
+        return PositionResource::make(
+            $position
+        )->additional([
+            'message' => __('Position retrieved successfully'),
+            'status' => Response::HTTP_OK
+        ]);
     }
 }
